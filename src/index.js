2749e8a893022dad409fa9d087f4c235558a95b7
maxvanbel <maxvanbel@gmail.com>
2017-04-24 22:33:46 -0500

    api data use

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBa432OSP8pH4eu0QT8mXj_WBqQH89jp2o';
+
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
 // create a new component that creates some HTML
 const App = () => {
   return (
